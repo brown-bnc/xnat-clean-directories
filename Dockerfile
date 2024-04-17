b7e6171a4fcacffc3f902c01a51ad473478c300d
Ford McDonald <ford_mcdonald@brown.edu>
2024-04-17 17:32:38 -0400

    scale days to clean by usage percentage

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-
 ARG UBUNTU_VERSION=22.04
 FROM ubuntu:$UBUNTU_VERSION
 ENV PARENT_PATH=/data/xnat
 ENV DAYS_TO_CLEAN=60
 
-# Clean logs files older than $DAYS_TO_CLEAN days
-CMD  /usr/bin/find $PARENT_PATH/home/logs -type f -name '*.log.*' -mtime +$DAYS_TO_CLEAN -delete\ 
-     && /usr/bin/find $PARENT_PATH/cache -type f -mtime +$DAYS_TO_CLEAN  -delete\
-     && /usr/bin/find $PARENT_PATH/build -type f -mtime +$DAYS_TO_CLEAN -delete
+COPY cleanup.sh /cleanup.sh
+RUN chmod +x /cleanup.sh
+
+CMD ["/cleanup.sh"]
---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+counter=0
+while IFS= read -r line; do
+        ((counter++))
+        if [ $counter -eq 2 ]; then
+                usage=$(echo "$line" | awk '{print $5}')
+                usage=${usage%\%}
+                DAYS_TO_CLEAN=$((DAYS_TO_CLEAN / usage))
+        fi
+done
+
+/usr/bin/find $PARENT_PATH/home/logs -type f -name '*.log.*' -mtime +$DAYS_TO_CLEAN -delete
+/usr/bin/find $PARENT_PATH/cache -type f -mtime +$DAYS_TO_CLEAN  -delete
+/usr/bin/find $PARENT_PATH/build -type f -mtime +$DAYS_TO_CLEAN -delete
