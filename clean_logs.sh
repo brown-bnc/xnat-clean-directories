0626b509dcef0d8101bbadb66d2f32379800852d
Camilo Diaz <camilo_diaz@brown.edu>
2025-05-05 10:09:17 -0400

    maint: added if case to skip data/xnat/archives as a relative path

--- clean_logs.sh
+++ clean_logs.sh
@@ -12,10 +12,12 @@ for rel_path in "${DIRS_TO_CLEAN[@]}"; do
   full_path="$PARENT_PATH/${rel_path}"
   
   # DONT DELETE /data/xnat/archives
-  if [[ "$rel_path" == "archives" || "$full_path" == "/data/xnat/archives" ]]; then
+  if [[ "$rel_path" == "archives" || \
+        "$full_path" == "/data/xnat/archives" || \
+        "$full_path" == "data/xnat/archives" ]]; then
     continue
   fi
-
+  
   if [[ "$rel_path" == "home/logs" ]]; then
     # Cleaning logs
     find "$full_path" -mindepth 1 -type f -name '*.log.*' -mtime +"$DAYS_TO_CLEAN" -delete
