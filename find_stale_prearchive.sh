7bc3b97aeb62678b5daef644f1cd8331d759f5c4
Camilo Diaz <k.diaz99@gmail.com>
2025-05-29 11:58:09 -0400

    maint: adding arguments to xargs command to avoid calling the du command with empty results

--- find_stale_prearchive.sh
+++ find_stale_prearchive.sh
@@ -2,6 +2,6 @@
 FOLDER="$PARENT_PATH/prearchive"
 
 # files older than 1 day
-mapfile -t files < <(find "$FOLDER" -type f -mtime +0 -exec dirname {} \; | sort -u | xargs -d '\n' du -sh 2>/dev/null)
+mapfile -t files < <(find "$FOLDER" -type f -mtime +0 -exec dirname {} \; | sort -u | grep -v '^$' | xargs -r  -d '\n' du -sh 2>/dev/null)
 
 python3 send_notification.py "${files[@]}"
